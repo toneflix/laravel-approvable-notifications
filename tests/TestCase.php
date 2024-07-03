78ae535a6d02a4a5e7fc8e6efb9b415202dc3c00
3m1n3nc3 <3m1n3nc3@users.noreply.github.com>
2024-07-03 01:18:44 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -26,7 +26,7 @@ abstract class TestCase extends Orchestra
 
     protected function defineDatabaseMigrations()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }
 
     protected function setUp(): void
@@ -34,10 +34,10 @@ abstract class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'ToneflixCode\\ApprovableNotifications\\Tests\\Database\\Factories\\' .
+            fn (string $modelName) => 'ToneflixCode\\ApprovableNotifications\\Tests\\Database\\Factories\\'.
                 class_basename(
                     $modelName
-                ) . 'Factory'
+                ).'Factory'
         );
     }
 
