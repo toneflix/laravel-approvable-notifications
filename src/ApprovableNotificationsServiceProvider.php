d5cae5ce3f9fc10136ce8aa8bf43b379357ee22a
3m1n3nc3 <mygames.ng@gmail.com>
2024-07-28 19:08:35 +0100

    Improve migration

--- src/ApprovableNotificationsServiceProvider.php
+++ src/ApprovableNotificationsServiceProvider.php
@@ -16,23 +16,31 @@ class ApprovableNotificationsServiceProvider extends ServiceProvider
         /*
          * Optional methods to load your package assets
          */
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        if ($this->unmigrated('create_approvable_notifications_table')) {
+            $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        }
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('approvable-notifications.php'),
+                __DIR__ . '/../config/config.php' => config_path('approvable-notifications.php'),
             ], 'approvable-notifications');
 
-            $canMigrate = empty(array_filter(File::files(base_path('database/migrations')), function (SplFileInfo $file) {
-                return str($file->getBasename())->contains('create_approvable_notifications_table');
-            }));
-
-            if ($canMigrate) {
+            if ($this->unmigrated('create_approvable_notifications_table')) {
                 $this->publishes([
-                    __DIR__.'/../database/migrations/2024_06_11_172655_create_approvable_notifications_table.php' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_approvable_notifications_table.php'),
+                    __DIR__ . '/../database/migrations/2024_06_11_172655_create_approvable_notifications_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_approvable_notifications_table.php'),
                 ], 'approvable-notifications');
             }
         }
+    }
+
+    protected function unmigrated(string $table)
+    {
+        return empty(array_filter(
+            File::files(base_path('database/migrations')),
+            function (SplFileInfo $file) use ($table) {
+                return str($file->getBasename())->contains($table);
+            }
+        ));
     }
 
     /**
@@ -41,7 +49,7 @@ class ApprovableNotificationsServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'approvable-notifications');
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'approvable-notifications');
 
         // Register the main class to use with the facade
         $this->app->singleton('approvable-notifications', function () {
